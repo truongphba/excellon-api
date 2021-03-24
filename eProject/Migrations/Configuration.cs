a7497684b695c794f609551c4b5b1671e12a21fb
Macbook <nguyendangbanghn93@gmail.com>
2021-03-24 20:18:50 +0700

    seed all

--- eProject/Migrations/Configuration.cs
+++ eProject/Migrations/Configuration.cs
@@ -34,6 +34,88 @@
             context.Database.ExecuteSqlCommand("DELETE FROM [Services]");
             context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Services', RESEED, 0)");
 
+            //Client_________________________________________________________________
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 1,
+                Name = "Long Vân",
+                PhoneNumber = "0987654321",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 2,
+                Name = "Bizman",
+                PhoneNumber = "0987654322",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 3,
+                Name = "Goldsun",
+                PhoneNumber = "0987654323",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 4,
+                Name = "Bibica",
+                PhoneNumber = "0987654324",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 5,
+                Name = "Hoàng phúc",
+                PhoneNumber = "0987654325",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 6,
+                Name = "Đại phát",
+                PhoneNumber = "0987654326",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 7,
+                Name = "Hòa phát",
+                PhoneNumber = "0987654327",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 8,
+                Name = "Kim tín",
+                PhoneNumber = "0987654328",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 9,
+                Name = "Đại Hải",
+                PhoneNumber = "0987654329",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.Clients.AddOrUpdate(new Client()
+            {
+                Id = 10,
+                Name = "Hoàng Long",
+                PhoneNumber = "0987654320",
+                Address = "Long Biên, Hà Nội",
+                Status = ClientStatus.Active
+            });
+            context.SaveChanges();
             //Department_________________________________________________________________
             context.Departments.AddOrUpdate(new Department()
             {
@@ -188,6 +270,90 @@
                 Address = "Gia Lâm, Hà Nội",
                 DepartmentId = 1,
                 Status = EmployeeStatus.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.SaveChanges();
+            //Payment___________________________________________________________________
+            context.Products.AddOrUpdate(new Product()
+            {
+                Id = 1,
+                Name = "Quảng cáo ngoài trời",
+                Description = "Cho thuê biển quảng cáo ngoài trời",
+                ClientId = 2,
+                Status = ProductService.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Products.AddOrUpdate(new Product()
+            {
+                Id = 2,
+                Name = "Bánh chưng",
+                Description = "Bánh chưng nhân thịt lợn",
+                ClientId = 3,
+                Status = ProductService.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Products.AddOrUpdate(new Product()
+            {
+                Id = 3,
+                Name = "Ghế gấp",
+                Description = "Sản xuất kinh doanh bàn ghê",
+                ClientId = 4,
+                Status = ProductService.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Products.AddOrUpdate(new Product()
+            {
+                Id = 4,
+                Name = "Cặp học sinh",
+                Description = "Sản xuất cặp da túi sách",
+                ClientId = 5,
+                Status = ProductService.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Products.AddOrUpdate(new Product()
+            {
+                Id = 5,
+                Name = "Bao cao su",
+                Description = "Bao cao hen mết siêu bền",
+                ClientId = 6,
+                Status = ProductService.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.SaveChanges();
+            //Service______________________________________________________________________
+            context.Services.AddOrUpdate(new Service()
+            {
+                Id = 1,
+                Name = "In-bound",
+                Price = 4500,
+                DepartmentId = 1,
+                Status = ServiceStatus.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Services.AddOrUpdate(new Service()
+            {
+                Id = 2,
+                Name = "Out-bound",
+                Price = 6000,
+                DepartmentId = 1,
+                Status = ServiceStatus.Active,
+                CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+                UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
+            });
+            context.Services.AddOrUpdate(new Service()
+            {
+                Id = 1,
+                Name = "Telemarketing",
+                Price = 5500,
+                DepartmentId = 1,
+                Status = ServiceStatus.Active,
                 CreatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
                 UpdatedAt = DateTime.Now.AddDays(new Random().Next(-10, -5)),
             });
