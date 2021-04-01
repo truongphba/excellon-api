76d04677fba37ac67850300a00dfedeb2c4b65e3
vuonghathanh <vuongthanh0111@gmail.com>
2021-04-01 14:48:51 +0700

    add ExportExcel api

--- eProject/Controllers/EmployeesController.cs
+++ eProject/Controllers/EmployeesController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
+using ClosedXML.Excel;
 using eProject.Models;
 using PagedList;
+using System.Web.Mvc;
 
 namespace eProject.Controllers
 {
@@ -38,6 +43,46 @@ namespace eProject.Controllers
                 data,
                 total
             });
+        }
+
+        [System.Web.Http.Route("api/Employees/Export")]
+        [System.Web.Http.HttpGet]
+        public void ExportToExcel(int limit, int? page, string keyword = "", int? status = null)
+        {
+            var employees = from s in db.Employees select s;
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                employees = employees.Where(s => s.UserName.Contains(keyword) || s.Email.Contains(keyword));
+            }
+            if (status.HasValue)
+            {
+                employees = employees.Where(s => s.Status == (EmployeeStatus)status);
+            }
+            int pageNumber = (page ?? 1);
+            var data = employees.Include(c => c.Department).OrderByDescending(s => s.CreatedAt).ToPagedList(pageNumber, limit);
+            DataTable dt = new DataTable("Employee");
+            dt.Columns.AddRange(new DataColumn[7]
+            {
+                new DataColumn("Name"),
+                new DataColumn("Email"),
+                new DataColumn("Phone Number"),
+                new DataColumn("Address"),
+                new DataColumn("Department"),
+                new DataColumn("Create At"),
+                new DataColumn("Status"),
+            });
+
+            foreach(Employee employee in data)
+            {
+                dt.Rows.Add(employee.UserName, employee.Email, employee.PhoneNumber, employee.Address, employee.Department, employee.CreatedAt, employee.Status);
+            }
+
+            XLWorkbook wb = new XLWorkbook();
+            wb.AddWorksheet(dt);
+            FileStream fs = File.OpenWrite("C:\\Users\\HP\\test2\\thanhancut.xlsx");
+            wb.SaveAs(fs);
+    
+           
         }
 
         // GET: api/Employees/5
