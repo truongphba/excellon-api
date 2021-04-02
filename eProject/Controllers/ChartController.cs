8e269f03dfb575eb7138c02d323427e6f1a6e829
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2021-04-02 19:24:16 +0700

    chart

---
+++ eProject/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+﻿using eProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eProject.Controllers
+{
+    public class ChartController : ApiController
+    {
+        private ExcelDbContext db = new ExcelDbContext();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetPayments()
+        {
+            var payments = from s in db.Payments
+                           select s;
+
+            var data = payments.Select(s => new { s.TotalCost, s.CreatedAt });
+            return Ok(new
+            {
+                data
+            });
+        }
+    }
+}
